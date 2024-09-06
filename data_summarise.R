ef1a0dccd7896595ecf5812f7eb3e8b2c7a31983
Andrii Tsokol <atsokol@gmail.com>
2024-09-06 17:20:07 +0200

    First commit

---
+++ data_summarise.R
@@ -0,0 +1,70 @@
+library(arrow)
+library(lubridate)
+
+data <- open_dataset("raw-data/parquet")
+
+# filter relevant codes
+all_codes <- data %>% 
+  group_by(OPER_CODE, OPER_NAME) %>% 
+  count() %>% 
+  arrange(OPER_CODE) %>% 
+  collect()
+
+primary_codes_table <- all_codes %>% 
+  filter(str_detect(OPER_NAME, "ПЕРВИННА")) 
+
+primary_codes <- primary_codes_table$OPER_CODE %>% unique()
+primary_codes <- c(primary_codes, 70, 71, 72)
+
+# BEV and PHEV codes
+bev_codes <- c("ЕЛЕКТРО")
+phev_codes <- c("БЕНЗИН, ГАЗ АБО ЕЛЕКТРО", "ЕЛЕКТРО АБО ДИЗЕЛЬНЕ ПАЛИВО", 
+                "ЕЛЕКТРО АБО БЕНЗИН")
+ev_codes <- c(bev_codes, phev_codes)
+
+# Calculate total car registrations
+cars <- data |> 
+  filter(OPER_CODE %in% primary_codes,
+         KIND == "ЛЕГКОВИЙ") |> 
+  mutate(DATE = floor_date(D_REG, "month"),
+         YEAR = year(D_REG),
+         FUEL = case_when(
+           FUEL %in% bev_codes ~ "BEV",
+           FUEL %in% phev_codes ~ "PHEV",
+           TRUE ~ "ICE")
+  ) |> 
+  mutate(AGE = YEAR - MAKE_YEAR) |> 
+  group_by(DATE, BRAND, FUEL) |> 
+  summarise(count = n()) |> 
+  collect()  |> 
+  mutate(BRAND = str_extract(BRAND, "\\S+")) |> 
+  group_by(DATE, BRAND, FUEL) |> 
+  summarise(count = sum(count)) |> 
+  rename(date = DATE, brand = BRAND, fuel = FUEL)
+
+write_csv(cars, "output-data/Ukraine_cars.csv")
+
+cars |> 
+  mutate(year = year(date)) |> 
+  group_by(year) |> 
+  summarise(count = sum(count))
+
+
+ev_cars <- data |> 
+  filter(OPER_CODE %in% primary_codes,
+         FUEL %in% ev_codes,
+         KIND == "ЛЕГКОВИЙ") |> 
+  mutate(DATE = floor_date(D_REG, "month"),
+         YEAR = year(D_REG),
+         FUEL = case_when(
+           FUEL %in% bev_codes ~ "BEV",
+           FUEL %in% phev_codes ~ "PHEV",
+           TRUE ~ "ICE")
+  ) |> 
+  mutate(AGE = YEAR - MAKE_YEAR) |> 
+  collect() |> 
+  mutate(BRAND = str_extract(BRAND, "\\S+")) |> 
+  rename(date = DATE, brand = BRAND, fuel = FUEL) |> 
+  select(brand, fuel, year = YEAR, age = AGE)
+
+write_csv(ev_cars, "output-data/Ukraine_EV_cars.csv")
---
+++ data_update.R
@@ -0,0 +1,87 @@
+library(readr)
+library(dplyr)
+library(stringr)
+library(arrow)
+library(lubridate)
+library(jsonlite)
+library(here)
+library(fs)
+
+# Data download
+# Folder with parquet database
+link_dir <- here("raw-data/parquet")
+
+# Download data passport from source
+pp <- 'https://data.gov.ua/dataset/06779371-308f-42d7-895e-5a39833375f0/datapackage'
+pass <- jsonlite::read_json(pp, simplifyVector = TRUE)
+urls <- pass$resources
+
+year <- urls$name |> tail(1) |> str_extract("([0-9]{4})$", group = 1) |> as.numeric()
+path <- urls$path |> tail(1) 
+file <- tempfile()
+download.file(path, file)
+new_file <- unzip(file, exdir = tempdir())
+
+# Read in data
+new_data <- read_delim(new_file, 
+                       delim=";", 
+                       col_types = cols_only(
+                         PERSON = col_character(),
+                         REG_ADDR_KOATUU = col_character(),
+                         OPER_CODE = col_integer(),
+                         OPER_NAME = col_character(), 
+                         D_REG = col_character(),
+                         DEP_CODE = col_character(),
+                         DEP = col_character(),
+                         BRAND = col_character(), 
+                         MODEL = col_character(), 
+                         VIN = col_character(),
+                         MAKE_YEAR = col_integer(), 
+                         COLOR = col_character(),
+                         KIND = col_character(), 
+                         BODY = col_character(),
+                         PURPOSE = col_character(),
+                         FUEL = col_character(),
+                         CAPACITY = col_double(),
+                         OWN_WEIGHT = col_double(),
+                         TOTAL_WEIGHT = col_double(),
+                         N_REG_NEW = col_character(),
+                       ),
+                       locale = locale(encoding = "UTF-8", decimal_mark = ",")
+                       ) |> 
+  select(-VIN)
+
+# Function to write parquet file to correct folder creating one if it doesn't exist
+write_parquet_dir <- function(df, year, link){
+  
+  # Find folder with the latest available year
+  current_max_year <- dir_ls(here(link), type = "directory") |> 
+    str_extract("([0-9]{4})$", group = 1) |> 
+    as.numeric() |> 
+    max()  
+  
+  # Year of new data
+  new_year <- df |> select({{year}}) |> max()
+  
+  # Write file to folder 
+  if(new_year == current_max_year) {
+    write_parquet(df, 
+                  here(link, paste0("YEAR=", new_year), "part-0.parquet"))
+  } else {
+    # create new directory if it doesn't exist
+    dir_create(here(link, paste0("YEAR=", new_year)))
+    write_parquet(df, 
+                  here(link, paste0("YEAR=", new_year), "part-0.parquet"))
+  }
+}
+
+new_data %>% 
+  mutate(D_REG = lubridate::parse_date_time2(D_REG, orders = c("%d.%m.%Y", "%d.%m.%y"))) |> 
+  distinct() |> 
+  mutate(YEAR = year(D_REG)) |>  
+  write_parquet_dir(year = YEAR, 
+                    link = link_dir)
+
+
+
+
