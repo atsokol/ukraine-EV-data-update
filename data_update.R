689dfe87f0d9bd08d9d43b037be8b5b99ab2bb47
Andrii Tsokol <atsokol@gmail.com>
2025-04-04 09:50:26 +0200

    temporary update to get data for 12m 2024

--- data_update.R
+++ data_update.R
@@ -16,8 +16,8 @@ pp <- 'https://data.gov.ua/dataset/06779371-308f-42d7-895e-5a39833375f0/datapack
 pass <- jsonlite::read_json(pp, simplifyVector = TRUE)
 urls <- pass$resources
 
-year <- urls$name |> tail(1) |> str_extract("([0-9]{4})$", group = 1) |> as.numeric()
-path <- urls$path |> tail(1) 
+year <- urls$name |> tail(2) |> str_extract("([0-9]{4})$", group = 1) |> as.numeric()
+path <- urls$path |> tail(2) 
 file <- tempfile()
 download.file(path, file)
 new_file <- unzip(file, exdir = tempdir())
@@ -64,7 +64,7 @@ write_parquet_dir <- function(df, year, link){
   new_year <- df |> select({{year}}) |> max()
   
   # Write file to folder 
-  if(new_year == current_max_year) {
+  if(new_year == current_max_year - 1) {
     write_parquet(df, 
                   here(link, paste0("YEAR=", new_year), "part-0.parquet"))
   } else {
